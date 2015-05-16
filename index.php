cbf808df43c6bc0a6582882fbb485bd6b3e1072f
Joannou1 <JohnnyAnimator@aol.com>
2015-05-15 17:20:31 -0700

    Header logo

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 <body>
 <div id="main-wrapper">
 	<header>
+		<div id="logo">                    
+			<a href="http://www.arma3-life.com/forums" title="Go to community index" rel="home" accesskey="1"><img src="http://www.arma3-life.com/forums/public/style_images/A3L_v2/life-studios64.png" alt="Logo"></a>
+		</div>
 	</header>
 </div>
 </body>
