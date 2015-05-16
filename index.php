2ce5a023a7b082459ddfba38f6f11e86bf23dc38
Joannou1 <JohnnyAnimator@aol.com>
2015-05-15 17:39:00 -0700

    Body wrapper + Header logo change

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300,400' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div id="main-wrapper">
 	<header>
-		<div id="logo">                    
-			<a href="http://www.arma3-life.com/forums" title="Go to community index" rel="home" accesskey="1"><img src="http://www.arma3-life.com/forums/public/style_images/A3L_v2/life-studios64.png" alt="Logo"></a>
+		<div id="body-wrapper">
+			<div id="logo">                    
+				ARMA3-LIFE
+			</div>
 		</div>
 	</header>
 </div>
